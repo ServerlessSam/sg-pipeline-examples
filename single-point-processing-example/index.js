6c82976f70abe3a2310b37e40a3d97b5e372894d
ServerlessSam <samuel.t.lock@gmail.com>
2023-03-29 08:17:51 -0600

    Update index.js

--- single-point-processing-example/index.js
+++ single-point-processing-example/index.js
@@ -1,5 +1,13 @@
 
 
+const dogOrCat = (bucketName, key) => {
+  return "dog"
+}
+
+const resize = (bucketName, key) => {
+  return true
+}
+
 const handler = async (event) => {
   let statusCode = 200;
   let message = "hello world!";
