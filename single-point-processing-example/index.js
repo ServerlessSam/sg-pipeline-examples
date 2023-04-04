1462d17a81fe83af67aebd9cd7038b53103d929a
Lorenzo Hidalgo Gadea <lorenzo@lhidalgo.dev>
2023-04-04 18:45:14 +0200

    minor fixes

--- single-point-processing-example/index.js
+++ single-point-processing-example/index.js
@@ -1,11 +1,14 @@
-const { RekognitionClient, DetectLabelsCommand } = require("@aws-sdk/client-rekognition")
+const {
+  RekognitionClient,
+  DetectLabelsCommand,
+} = require("@aws-sdk/client-rekognition");
 
 /**
  * Used to identify if a PNG is a dog or cat
  *
  * @param {String} bucket_name name of S3 bucket
  * @param {String} key key name within bucket (including prefix)
- * @returns {String} string of "Dog", "Cat" or "Neither"
+ * @returns {Promise<String>} string of "Dog", "Cat" or "Neither"
  */
 async function dogOrCat(bucket_name, key) {
   const client = new RekognitionClient({ region: "us-east-1" });
@@ -20,25 +23,23 @@ async function dogOrCat(bucket_name, key) {
     MinConfidence: 50,
     Settings: {
       GeneralLabels: {
-        LabelInclusionFilters: [
-          "Dog", "Cat"
-        ]
-      }
-    }
-  }
+        LabelInclusionFilters: ["Dog", "Cat"],
+      },
+    },
+  };
   const command = new DetectLabelsCommand(params);
-  
-  data = await client.send(command)
+
+  const data = await client.send(command);
 
   if (data.Labels.length > 0) {
-    console.log(`I am ${data.Labels[0].Confidence}% sure this is a ${data.Labels[0].Name}!`)
-    return data.Labels[0].Name
-  }
-  else {
-    return "Neither"
+    console.log(
+      `I am ${data.Labels[0].Confidence}% sure this is a ${data.Labels[0].Name}!`
+    );
+    return data.Labels[0].Name;
+  } else {
+    return "Neither";
   }
 }
-
 
 /**
  * Used to resize a PNG to 400x400
@@ -49,8 +50,8 @@ async function dogOrCat(bucket_name, key) {
  * @returns {String} full arn of new object in S3
  */
 const resize_and_save = (bucket_name, key, animal) => {
-  return "arn:aws:s3:::foo/bar"
-}
+  return "arn:aws:s3:::foo/bar";
+};
 
 /**
  * Used to write a record to the RDS DB
@@ -59,26 +60,27 @@ const resize_and_save = (bucket_name, key, animal) => {
  * @param {String} animal either "dog" or "cat". This should be the prefix within the S3 bucket
  */
 const persist_record_to_db = (obj_arn, animal) => {
-  return
-}
+  return;
+};
 
 const handler = async (event) => {
-
   // Get the object from the event and show its content type
   const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-  
-  const animal = await dogOrCat(bucket, key)
-  const new_resized_obj_arn = resize_and_save(bucket, key, animal)
-  persist_record_to_db(new_resized_obj_arn, animal)
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
 
-  const msg = `Image of a ${animal} saved at ${new_resized_obj_arn}!`
+  const animal = await dogOrCat(bucket, key);
+  const new_resized_obj_arn = resize_and_save(bucket, key, animal);
+  persist_record_to_db(new_resized_obj_arn, animal);
+
+  const msg = `Image of a ${animal} saved at ${new_resized_obj_arn}!`;
 
   return {
-    body: msg
+    body: msg,
   };
 };
 
 module.exports = {
-  handler
+  handler,
 };
