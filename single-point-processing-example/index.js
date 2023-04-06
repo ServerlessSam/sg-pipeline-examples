60830176a4f44aa930c1659a1be415320602ad28
Lorenzo Hidalgo Gadea <lorenzo@lhidalgo.dev>
2023-04-06 11:08:05 +0200

    test sharp setup

--- single-point-processing-example/index.js
+++ single-point-processing-example/index.js
@@ -1,7 +1,15 @@
+'use strict';
+
 const {
   RekognitionClient,
   DetectLabelsCommand,
 } = require("@aws-sdk/client-rekognition");
+
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+
+const { Upload } = require("@aws-sdk/lib-storage");
+
+const Sharp = require('sharp');
 
 /**
  * Used to identify if a PNG is a dog or cat
@@ -11,7 +19,7 @@ const {
  * @returns {Promise<String>} string of "Dog", "Cat" or "Neither"
  */
 async function dogOrCat(bucket_name, key) {
-  const client = new RekognitionClient({ region: "us-east-1" });
+  const client = new RekognitionClient({ region: process.env.REGION });
 
   const params = {
     Image: {
@@ -47,10 +55,39 @@ async function dogOrCat(bucket_name, key) {
  * @param {String} bucket_name name of S3 bucket
  * @param {String} key key name within bucket (including prefix)
  * @param {String} animal either "dog" or "cat". This should be the prefix within the S3 bucket
- * @returns {String} full arn of new object in S3
+ * @returns {Promise<String>} full arn of new object in S3
  */
-const resize_and_save = (bucket_name, key, animal) => {
-  return "arn:aws:s3:::foo/bar";
+const resize_and_save = async (bucket_name, key, animal) => {
+  try {
+    console.log("Entered resize");
+    console.log(bucket_name, key);
+    const client = new S3Client({ region: process.env.REGION });
+    const input = {
+      Bucket: bucket_name,
+      Key: key,
+    };
+    const getCommand = new GetObjectCommand(input);
+    const file = await client.send(getCommand);
+    console.log("Fetched File");
+    const streamResize = Sharp()
+          .resize(400, 400)
+          .toFormat('png')
+    const upload = new Upload({
+      client: client,
+      params: {
+        Bucket: bucket_name,
+        Key: "processed/" + animal + "/" + key.split("/").slice(-1)[0],
+        Body: file.Body.pipe(streamResize),
+        ContentType: "image/png",
+      },
+    });
+    const response = await upload.done();
+    console.log(response);
+    return "arn:aws:s3:::" + response.Bucket + "/" + response.Key;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
 /**
@@ -64,14 +101,16 @@ const persist_record_to_db = (obj_arn, animal) => {
 };
 
 const handler = async (event) => {
+  console.log(event);
   // Get the object from the event and show its content type
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(
     event.Records[0].s3.object.key.replace(/\+/g, " ")
   );
 
-  const animal = await dogOrCat(bucket, key);
-  const new_resized_obj_arn = resize_and_save(bucket, key, animal);
+  //const animal = await dogOrCat(bucket, key);
+  const animal = "dog";
+  const new_resized_obj_arn = await resize_and_save(bucket, key, animal);
   persist_record_to_db(new_resized_obj_arn, animal);
 
   const msg = `Image of a ${animal} saved at ${new_resized_obj_arn}!`;
